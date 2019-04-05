a38dbc4b353fd37f8dcdb007fc2cbdc5e49d123d
TracyBaraza <barazatracy16@gmal.com>
2019-04-06 01:22:43 +0300

    more calculations

--- calculator.py
+++ calculator.py
@@ -1,5 +1,36 @@
 def add(a,b):
-	answer =a+b
+	answer = a+b
 	return answer
 
+
+def subtract(c,d):
+	answer = c-d
+	return answer
+
+
+def multiply(e,f):
+	answer = e*f
+	return answer
+
+
+def division(g,h):
+	answer = g/h
+	return answer
+
+
+def whole_numberdivision(i,j):
+	answer = i//j
+	return answer
+
+
+def modulus(k,l):
+	answer = k%l
+	return answer
+
+
+def exponent(m,n):
+	answer = m**n
+	return answer
+	
+			
 
